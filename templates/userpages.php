8ffd1325690b8757238d1684e9f9be16d3446bfc
AndreFerGon <andrefgoncalves02@gmail.com>
2023-12-09 18:31:49 +0000

    .

---
+++ templates/userpages.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="menu">
+        <ul>
+            <li><a href="#">Personal Area</a></li>
+            <li><a href="#">My Orders</a></li>
+            <li><a href="#">My Returns</a></li>
+            <li><a href="#">My Repairs</a></li>
+        </ul>
+    </div>
+</body>
--- user.php
+++ user.php
@@ -4,23 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Menu Vertical</title>
 </head>
-<body>
-    <div class="menu">
-        <ul>
-            <li><a href="#">Personal Area</a></li>
-            <li><a href="#">My Orders</a></li>
-            <li><a href="#">My Returns</a></li>
-            <li><a href="#">My Repairs</a></li>
-        </ul>
-    </div>
-</body>
 </html>
   <body>
     <?php
    include_once('templates/header&navmenu.php');
    include_once('templates/footer.php');
+   include_once('templates/userpages.php');
+
     ?>
   </body>
 </html>
