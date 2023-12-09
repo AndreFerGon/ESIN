c519e5ce5ed02a0b40d51576e74d19d4bc161cda
Kikam19 <kikabreu@live.com>
2023-12-09 17:06:51 +0000

    texto pagina sobre

--- informaçãolegal.php
+++ informaçãolegal.php
@@ -47,23 +47,7 @@
       <p>
       A LOJA DE TECNOLOGIA compromete-se a operar de acordo com os mais elevados padrões éticos e legais, proporcionando 
       aos clientes uma experiência de compra segura, transparente e satisfatória.
-      </p>
-      
-      </h3>Para mais informações</h3>
-      <p>
-      <form action="process_form.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-        
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" rows="4" required></textarea>
-        
-        <input type="submit" value="Submit">
-      </form>
-      </p>   
+  
     </div>  
    
 
--- sobre.php
+++ sobre.php
@@ -2,55 +2,58 @@
 <html lang="en">
   <body>
     <div id="customer-support">
-      <h2>Apoio ao cliente</h2>
+      <h2>Sobre</h2>
       <p>
-        Na LOJA DE TECNOLOGIA, entendemos que a espera pela chegada dos seus produtos é sempre ansiosa. 
-        É por isso que nos empenhamos em proporcionar um envio rápido e eficiente para todos os cantos de Portugal. 
-        E a melhor parte? Os portes são completamente gratuitos! Não queremos que preocupações com custos de entrega 
-        atrapalhem a sua experiência de compra.
+       Na iTEC, estamos empenhados em redefinir a forma como as pessoas interagem com a tecnologia. Desde a nossa 
+       fundação, perseguimos a inovação constante e a excelência, com a missão de criar soluções tecnológicas que 
+       simplificam e aprimoram a sua vida diária.
       </p>
-      <h3>Trocas e Devoluções Descomplicadas</h3>
+      <h3>A Nossa História:</h3>
       <p>
-        Queremos que cada compra seja perfeita, por isso, oferecemos um processo de troca e devolução descomplicado. 
-        Se por acaso não ficar completamente satisfeito com o seu produto, temos uma política flexível que permite 
-        trocas e devoluções dentro de um prazo razoável. A sua satisfação é a nossa prioridade, e estamos aqui para 
-        garantir que cada compra seja uma experiência positiva.
+      A iTEC foi concebida por um grupo visionário de empreendedores, movidos pela paixão pela inovação e pela 
+      convicção de que a tecnologia pode ser uma força positiva no mundo. Fundada há alguns anos, a empresa rapidamente 
+      se destacou no cenário tecnológico, lançando produtos que não apenas seguiam as tendências, mas as moldavam.
+      Desde a nossa génese, estabelecemos valores fundamentais que moldam tudo o que fazemos. A inovação é o nosso 
+      lema, a integridade é a nossa base, e o compromisso com a satisfação do cliente é a nossa bússola. 
+      Estes valores impulsionaram o nosso crescimento e continuam a guiar cada decisão que tomamos.
       </p>
-      <h3>Garantia de Qualidade em Todos os Produtos</h3>
+      <h3>A nossa ação</h3>
       <p>
-        Na LOJA DE TECNOLOGIA, a qualidade dos produtos é a nossa assinatura. Cada item em nosso catálogo é 
-        cuidadosamente selecionado para garantir o melhor em termos de desempenho, durabilidade e inovação tecnológica. 
-        Além disso, oferecemos garantia em todos os produtos para que você tenha total tranquilidade. Estamos comprometidos 
-        em fornecer apenas o melhor em tecnologia, respaldado pela nossa garantia de qualidade.
+        Na iTEC, a nossa oferta abrange uma ampla gama de produtos tecnológicos projetados para simplificar a sua vida. 
+        Desde dispositivos eletrónicos de última geração até soluções inteligentes para casa e escritório, cada produto 
+        é cuidadosamente concebido para oferecer funcionalidade intuitiva e desempenho excepcional.
+        Os nossos smartphones combinam design elegante com tecnologia avançada, proporcionando uma experiência fluida e 
+        envolvente. Os dispositivos inteligentes para casa transformam o seu espaço, proporcionando maior conforto e 
+        eficiência. E, para aqueles momentos em que a sua tecnologia precisa de assistência, oferecemos serviços de suporte 
+        técnico e reparação de confiança.
       </p>
-      <h3>Métodos de Pagamento Seguros e Convenientes</h3>
+      <h3>Compromisso com a Sustentabilidade</h3>
       <p>
-        Facilitamos o processo de pagamento para que a sua experiência de compra seja tão suave quanto possível. 
-        Aceitamos diversos métodos de pagamento seguros, desde cartões de crédito até opções de pagamento online. 
-        Na LOJA DE TECNOLOGIA, a segurança das suas transações é uma prioridade, e você pode confiar que os seus 
-        dados estão protegidos em todas as etapas do processo.
+        Consciente da responsabilidade ambiental, a iTEC incorporou práticas sustentáveis em todas as fases de produção. 
+        Desde a escolha de materiais até a gestão eficiente de recursos, a empresa busca constantemente minimizar seu impacto 
+        ambiental e promover uma abordagem sustentável para o desenvolvimento tecnológico.
+      </p>
+      <h3>Crescimento Global</h3>
+      <p>
+       À medida que a iTEC ganhou destaque no cenário tecnológico, expandiu suas operações para além das fronteiras nacionais.
+       A marca "iTEC" tornou-se sinónimo de qualidade, confiança e inovação em diversos mercados internacionais.
+      </p>
+      <h3>Futuro Promissor</h3>
+      <p>
+       Hoje, a iTEC continua a inovar, impulsionada por uma equipa dedicada de profissionais apaixonados. A empresa olha para 
+       o futuro com entusiasmo, mantendo-se comprometida em criar tecnologias que não apenas acompanhem, mas também antecipem 
+       as necessidades em constante evolução dos consumidores.
       </p>
       <p>
-        Na LOJA DE TECNOLOGIA, não vendemos apenas produtos tecnológicos; oferecemos uma experiência de compra 
-        completa, desde o momento em que faz o seu pedido até a entrega do produto à sua porta. Conte connosco para 
-        proporcionar tecnologia de ponta, serviços excepcionais e total tranquilidade em todas as suas transações.
-      </p>
-      </h3>Para mais informações</h3>
-      <p>
-      <form action="process_form.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-        
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" rows="4" required></textarea>
-        
-        <input type="submit" value="Submit">
-      </form>
-      </p>   
-    </div>  
+      A história da "iTEC" é um testemunho da perseverança, criatividade e dedicação à excelência que caracterizam essa jornada empreendedora.
+      </p>  
+    </div>
+    <div id="center">
+        <h2>Contactos</h2>
+        <p>
+          <a href="contacts.php">See more</a>
+        </p>
+      </div>  
    
 
     <?php
