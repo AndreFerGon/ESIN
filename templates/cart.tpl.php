566e0528a03cf1f173cdf8105597e1d14b73404e
madalenamartins256 <146451419+madalenamartins256@users.noreply.github.com>
2023-12-14 17:49:41 +0000

    Update cart.tpl.php

--- templates/cart.tpl.php
+++ templates/cart.tpl.php
@@ -1,35 +1,40 @@
 <section>
-    <h2>Shopping Cart</h2>
-  
-    <?php if(!isset($_SESSION['cart']) || count($_SESSION["cart"]) == 0) { ?>
+  <h2>Shopping Cart</h2>
+
+  <?php if (!isset($_SESSION['cart']) || count($_SESSION["cart"]) == 0) { ?>
       <p>No items</p>
-    <?php } else { ?>
-  
+  <?php } else { ?>
+
       <table id="cart-table">
-        <tr>
-          <th></th>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Quantity</th>
-        </tr>
-  
-        <?php foreach ($_SESSION['cart'] as $product) { ?>
-          <tr>
-            <td><img src="images/<?php echo $product['id'] ?>.png"></td>
-            <td><?php echo $product['id'] ?></td>
-            <td><?php echo $product['name'] ?></td>
-            <td><?php echo $product['price'] ?></td>
-            <td><?php echo $product['quantity'] ?></td>
-            <td><a href="addtocart.php?id=<?php echo $product['id']; ?>">X</a></td>
-          </tr>
-        <?php } ?>
+
+          <?php
+          $totalPrice = 0; // Inicializa o preço total
+
+          foreach ($_SESSION['cart'] as $product) {
+              $totalPrice += $product['price'] * $product['quantity']; // Soma o preço do produto ao total
+          ?>
+              <tr>
+                  <td><img src="images/products/<?php echo $product['id'] ?>.png"></td>
+                  <td><?php echo $product['id'] ?></td>
+                  <td><?php echo $product['model'] ?></td>
+                  <td><?php echo $product['price'] ?></td>
+                  <td><?php echo $product['quantity'] ?></td>
+                  <td>
+                      <form action="remove_from_cart.php" method="post">
+                          <input type="hidden" name="id" value="<?php echo $product['id']; ?>">
+                          <button type="submit" name="RemoveFromCart">Remove</button>
+                      </form>
+                  </td>
+              </tr>
+          <?php } ?>
       </table>
-  
+
+      <p>Total: <?php echo $totalPrice ?></p>
+
       <form action="action_checkout.php">
-        <button>Checkout</button>
+          <button>Checkout</button>
       </form>
-  
-    <?php } ?>
-  
-  </section>
+
+  <?php } ?>
+
+</section>
