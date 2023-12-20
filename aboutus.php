399d89c0db4096cd0f1e97bfa9b16538a2eb8445
AndreFerGon <andrefgoncalves02@gmail.com>
2023-12-20 15:29:18 +0000

    ta

--- aboutus.php
+++ aboutus.php
@@ -14,28 +14,28 @@ session_start();
     </p>
     </div>
   
-  <div id="our_history">
-    <h3>Our History:</h3>
-      <p>
-        iTEC was conceived by a visionary group of entrepreneurs, driven by a passion for innovation and the belief that technology can be a positive force in the world. Founded 6 years ago, the company quickly stood out in the technological landscape, launching products that not only followed trends but shaped them.
-        From our inception, we have established fundamental values that shape everything we do. Innovation is our motto, integrity is our foundation, and commitment to customer satisfaction is our compass. These values have propelled our growth and continue to guide every decision we make.
-      </p>
-  </div>  
+    <div id="our_history">
+      <h3>Our History:</h3>
+        <p>
+          iTEC was conceived by a visionary group of entrepreneurs, driven by a passion for innovation and the belief that technology can be a positive force in the world. Founded 6 years ago, the company quickly stood out in the technological landscape, launching products that not only followed trends but shaped them.
+          From our inception, we have established fundamental values that shape everything we do. Innovation is our motto, integrity is our foundation, and commitment to customer satisfaction is our compass. These values have propelled our growth and continue to guide every decision we make.
+        </p>
+    </div>  
 
-  <div id="our_mission">
-    <h3>Our Action</h3>
-      <p>
-      iTEC, offers range of technological products designed to simplify your life. From cutting-edge electronic devices to smart solutions for home and office, each product is carefully designed to offer intuitive functionality and exceptional performance.
-       Our smartphones blend elegant design with advanced technology, providing a smooth and engaging experience. Smart home devices transform your space, providing greater comfort and efficiency. For those moments when your technology needs assistance, we offer support and repair services.
-      </p>
-  </div>
+    <div id="our_mission">
+      <h3>Our Action</h3>
+        <p>
+        iTEC, offers range of technological products designed to simplify your life. From cutting-edge electronic devices to smart solutions for home and office, each product is carefully designed to offer intuitive functionality and exceptional performance.
+        Our smartphones blend elegant design with advanced technology, providing a smooth and engaging experience. Smart home devices transform your space, providing greater comfort and efficiency. For those moments when your technology needs assistance, we offer support and repair services.
+        </p>
+    </div>
 
-  <div id="our_vision">
-    <h3>Commitment to Sustainability</h3>
-      <p>
-       Conscious of environmental responsibility, iTEC has incorporated sustainable practices at every stage of production. From material choices to efficient resource management, the company constantly seeks to minimize its environmental impact and promote a sustainable approach to technological development.
-      </p>
-  </div>
+    <div id="our_vision">
+      <h3>Commitment to Sustainability</h3>
+        <p>
+        Conscious of environmental responsibility, iTEC has incorporated sustainable practices at every stage of production. From material choices to efficient resource management, the company constantly seeks to minimize its environmental impact and promote a sustainable approach to technological development.
+        </p>
+    </div>
 
   <div id="our_values">
     <h3>Global Growth</h3>
@@ -59,5 +59,5 @@ session_start();
   
     ?>
 
-    </body>
+  </body>
 </html>
